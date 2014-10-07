cbf9429f9f8fa3d5d16c4cb31c8d6c0ba72c239e
Xander Gerreman <xpj.gerreman@student.avans.nl>
2014-10-07 11:24:45 +0200

    De basis gelegd voor klant

--- src/facturatieSysteem/KlantenSubsysteem/EntityLayer/Klant.java
+++ src/facturatieSysteem/KlantenSubsysteem/EntityLayer/Klant.java
@@ -4,4 +4,15 @@ package facturatieSysteem.KlantenSubsysteem.EntityLayer;
 
 public class Klant {
 
+	private String BSN;
+	private String Naam;
+	private String Adres;
+	private String Postcode;
+	private String Woonplaats;
+	private date Geboortedatum;
+	
+	public Klant(){
+		
+	}
+	
 }
