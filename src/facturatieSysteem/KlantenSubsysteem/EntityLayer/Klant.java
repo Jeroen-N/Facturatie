bf04db4979de48b44b4f56c5412b2d86e5f34959
Xander Gerreman <xpj.gerreman@student.avans.nl>
2014-10-07 16:14:18 +0200

    Alles volgens klassediagram ingevoerd

--- src/facturatieSysteem/KlantenSubsysteem/EntityLayer/Klant.java
+++ src/facturatieSysteem/KlantenSubsysteem/EntityLayer/Klant.java
@@ -15,15 +15,19 @@ public class Klant {
 	private Date Geboortedatum;
 	private String TelefoonNr;
 	private String Email;
-	private String ResterendEigenRisico;
+	private double ResterendEigenRisico;
 	private String RekeningNr;
 	private VerzekeringPolis Verzekering;
 	private String Betaalwijze;
 
+	/*
+	 * Constructor voor de Klant klasse. Hiermee kan een klant aangemaakt worden
+	 * en de basisgegevens van deze klant neergezet worden
+	 */
 	public Klant(String BSN, String Naam, String Adres, String Postcode,
 			String Woonplaats, Date Geboortedatum, String TelefoonNr,
-			String Email, String ResterendEigenRisico, String RekeningNr,
-			VerzekeringPolis Verzekering, String Betaalwijze) {
+			String Email, String RekeningNr, VerzekeringPolis Verzekering,
+			String Betaalwijze) {
 		this.BSN = BSN;
 		this.Naam = Naam;
 		this.Adres = Adres;
@@ -32,31 +36,41 @@ public class Klant {
 		this.Geboortedatum = Geboortedatum;
 		this.TelefoonNr = TelefoonNr;
 		this.Email = Email;
-		this.ResterendEigenRisico = ResterendEigenRisico;
 		this.RekeningNr = RekeningNr;
 		this.Verzekering = Verzekering;
 		this.Betaalwijze = Betaalwijze;
 	}
 
-	public double BerekenTotaalEigenRisico(){
-		return 0;
-		
+	/*
+	 * In deze methode 
+	 */
+	public void setTotaalEigenRisico(double eigenRisico) {
+		ResterendEigenRisico = eigenRisico;
 	}
-	
-	public String SetIncassoOfFactuur(){
+
+	/*
+	 * In deze methode word het totaal eigen risico berekent, door gebruik te
+	 * maken het te declareren bedrag, wordt het totaal eigen risico verminderd.
+	 */
+	public double BerekenTotaalEigenRisico(double Bedrag) {
+		double eindBedrag;
+		eindBedrag = ResterendEigenRisico - Bedrag;
+		return eindBedrag;
+	}
+
+	/*
+	 * Wat houdt deze functie in?
+	 */
+	public String SetIncassoOfFactuur() {
 		return Email;
-		
 	}
-	
-	public VerzekeringPolis getVerzekering(){
+
+	/*
+	 * In deze methode wordt het verzekeringstype van de client opgevraagd. Dit
+	 * wordt gedaan door middel van de getType methode uit verzekeringsPolis
+	 */
+	public VerzekeringPolis getVerzekering() {
 		return Verzekering;
 	}
-	
-	public boolean updateKlant(String BSN, String Naam, String Adres, String Postcode,
-			String Woonplaats, Date Geboortedatum, String TelefoonNr,
-			String Email, String ResterendEigenRisico, String RekeningNr,
-			VerzekeringPolis Verzekering, String Betaalwijze){
-		
-	}
-	
+
 }
