a6255d2d778fe8a5e5734bd54596617ccfdfe979
Jeroen <jpgnuijt@avans.nl>
2014-10-07 17:30:11 +0200

    Verzekeringstype Entity

--- src/facturatieSysteem/VerzekeringSubsysteem/EntityLayer/Verzekeringstype.java
+++ src/facturatieSysteem/VerzekeringSubsysteem/EntityLayer/Verzekeringstype.java
@@ -1,5 +1,31 @@
+/* @author Jeroen Nuijten
+ * @version 0.1
+ * 
+ * Realiseert de verzekeringstype klasse, deze onderscheid de typen verzekeringen.
+ */
 package facturatieSysteem.VerzekeringSubsysteem.EntityLayer;
 
 public class Verzekeringstype {
-
+	private int VerzekeringsTypeID;
+	private int EigenRisico;
+	private int behandelcodes; //klopt dit? waarom is dit...
+	// Missen er attributen?
+	
+	/*
+	 * @param VerzekeringsTypeID Het ID van de verzekeringstype
+	 * @param EigenRisico Het eigen risico van het type verzekering
+	 * @param behandelcodes de behandelcodes
+	 */
+	public Verzekeringstype(int VerzekeringsTypeID, int EigenRisico, int behandelcodes){
+		this.VerzekeringsTypeID = VerzekeringsTypeID;
+		this.EigenRisico = EigenRisico;
+		this.behandelcodes = behandelcodes;
+	}
+	
+	/*
+	 * @return Het eigen risico van het verzekeringstype
+	 */
+	public int getEigenRisico(){
+		return EigenRisico;
+	}
 }
