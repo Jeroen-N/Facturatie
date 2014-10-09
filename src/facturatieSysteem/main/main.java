68e93c6429502e3add6e9b2dfeec134d8c9fab6c
Xander Gerreman <xpj.gerreman@student.avans.nl>
2014-10-09 12:28:09 +0200

    main HEAD opgelost

--- src/facturatieSysteem/main/main.java
+++ src/facturatieSysteem/main/main.java
@@ -5,15 +5,7 @@ import facturatieSysteem.FacturatieSubsysteem.PresentationLayer.FacturatieGUI;
 
 public class main {
 	public static void main(String[] args){
-<<<<<<< HEAD
 		MainGUI gui = new MainGUI();
-=======
-		//TODO: initialize GUI/managers
-		
-		FacturatieManagerImpl facturatieManagerImpl = new FacturatieManagerImpl();
-        
-        FacturatieGUI facturatieGui = new FacturatieGUI(facturatieManagerImpl);
 
->>>>>>> FacturatieSubSysteem
 	}
 }
