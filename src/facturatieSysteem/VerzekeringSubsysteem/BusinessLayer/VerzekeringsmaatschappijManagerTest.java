00cc0455d6b7e2758c7e61ad0bb2a4a33f997743
Jeroen <jpgnuijt@avans.nl>
2014-10-28 23:07:00 +0100

    no bats :elephant: :dash:

---
+++ src/facturatieSysteem/VerzekeringSubsysteem/BusinessLayer/VerzekeringsmaatschappijManagerTest.java
@@ -0,0 +1,23 @@
+package facturatieSysteem.VerzekeringSubsysteem.BusinessLayer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import facturatieSysteem.VerzekeringSubsysteem.EntityLayer.Verzekeringstype;
+
+public class VerzekeringsmaatschappijManagerTest {
+	private VerzekeringsmaatschappijManager instance;
+	@Before
+	public void setUp() throws Exception {
+		instance = new VerzekeringsmaatschappijManagerImpl();
+		
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
