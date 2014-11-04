66f3b1048285e2de93af61d0f5b51aee6452d243
Ricardo Jobse <jobse.ricardo@gmail.com>
2014-11-04 13:23:56 +0100

    check op kvk nummer
    alleen de check, nog geen confirm dialog

--- FacturatieSysteem/src/facturatieSysteem/VerzekeringSubsysteem/BusinessLayer/VerzekeringsmaatschappijManagerImpl.java
+++ FacturatieSysteem/src/facturatieSysteem/VerzekeringSubsysteem/BusinessLayer/VerzekeringsmaatschappijManagerImpl.java
@@ -16,7 +16,7 @@ public class VerzekeringsmaatschappijManagerImpl implements Verzekeringsmaatscha
 	private VerzekeringsmaatschappijDAO VerzekeringDAO = new VerzekeringsmaatschappijDAOImpl();
 	private VerzekeringstypeDAO VerzekeringtypeDAO = new VerzekeringsDAOImpl();
 	private String errorMessage;
-	
+
 	public VerzekeringsmaatschappijManagerImpl(){
 		ArrayList<Verzekeringsmaatschappij> lijst = VerzekeringDAO.getMaatschappijenXML();
 		this.importData(lijst);
@@ -30,30 +30,32 @@ public class VerzekeringsmaatschappijManagerImpl implements Verzekeringsmaatscha
 		}
 		return null;
 	}
-	
+
 	@Override
 	public boolean addVerzekeringsmaatschappij(Verzekeringsmaatschappij maatschappij) {
 		if(getVerzekeringsmaatschappij(maatschappij.getNr()) == null){
-			verzekeringsMaatschappijen.add(maatschappij);
-			VerzekeringDAO.addMaatschappijXML(maatschappij);
-			return true;
-		}
+			if(checkKvk(maatschappij) == true){
+				verzekeringsMaatschappijen.add(maatschappij);
+				VerzekeringDAO.addMaatschappijXML(maatschappij);
+				return true;
+			}}
 		return false;
 	}
 
 	@Override
 	public boolean updateVerzekeringsmaatschappij(Verzekeringsmaatschappij maatschappij) {
 		if(getVerzekeringsmaatschappij(maatschappij.getNr()) != null){
-			Verzekeringsmaatschappij maat = getVerzekeringsmaatschappij(maatschappij.getNr());
-			maat.setNaam(maatschappij.getNaam());
-			maat.setAdres(maatschappij.getAdres());
-			maat.setPostcode(maatschappij.getPostcode());
-			maat.setPlaats(maatschappij.getPlaats());
-			maat.setKVK(maatschappij.getKVK());
-			maat.setRekeningNR(maatschappij.getRekeningNR());
-			VerzekeringDAO.updateMaatschappijXML(maatschappij);
-			return true;
-		}
+			if(checkKvk(maatschappij) == true){
+				Verzekeringsmaatschappij maat = getVerzekeringsmaatschappij(maatschappij.getNr());
+				maat.setNaam(maatschappij.getNaam());
+				maat.setAdres(maatschappij.getAdres());
+				maat.setPostcode(maatschappij.getPostcode());
+				maat.setPlaats(maatschappij.getPlaats());
+				maat.setKVK(maatschappij.getKVK());
+				maat.setRekeningNR(maatschappij.getRekeningNR());
+				VerzekeringDAO.updateMaatschappijXML(maatschappij);
+				return true;
+			}}
 		return false;
 	}
 
@@ -65,12 +67,12 @@ public class VerzekeringsmaatschappijManagerImpl implements Verzekeringsmaatscha
 		}
 		return false;
 	}
-	
+
 	@Override
 	public ArrayList<Verzekeringsmaatschappij> getVerzekeringsmaatschappijen() {
 		return verzekeringsMaatschappijen;
 	}
-	
+
 	@Override
 	public Verzekeringstype getVerzekeringstype(Verzekeringsmaatschappij maatschappij, String nr) {
 		for(Verzekeringstype type : maatschappij.getTypes()){
@@ -80,7 +82,7 @@ public class VerzekeringsmaatschappijManagerImpl implements Verzekeringsmaatscha
 		}
 		return null;
 	}
-	
+
 	public Verzekeringstype getVerzekeringstypeByName(Verzekeringsmaatschappij maatschappij, String naam) {
 		for(Verzekeringstype type : maatschappij.getTypes()){
 			if(type.getNaam().equals(naam)){
@@ -89,7 +91,7 @@ public class VerzekeringsmaatschappijManagerImpl implements Verzekeringsmaatscha
 		}
 		return null;
 	}
-	
+
 	@Override
 	public void addVerzekeringstype(Verzekeringsmaatschappij maatschappij, Verzekeringstype type) {
 		if(getVerzekeringstype(maatschappij, type.getNr()) == null){
@@ -97,7 +99,7 @@ public class VerzekeringsmaatschappijManagerImpl implements Verzekeringsmaatscha
 			VerzekeringtypeDAO.addVerzekeringstypeXML(maatschappij.getNr(), type);
 		}
 	}
-	
+
 	@Override
 	public void updateVerzekeringstype(Verzekeringsmaatschappij maatschappij, Verzekeringstype type) {
 		if(getVerzekeringstype(maatschappij, type.getNr()) != null){
@@ -107,7 +109,7 @@ public class VerzekeringsmaatschappijManagerImpl implements Verzekeringsmaatscha
 			VerzekeringtypeDAO.updateVerzekeringstypeXML(maatschappij.getNr(), type);
 		}
 	}
-	
+
 	@Override
 	public boolean deleteVerzekeringstype(Verzekeringsmaatschappij maatschappij, Verzekeringstype type) {
 		if(getVerzekeringstype(maatschappij, type.getNr()) != null){
@@ -116,7 +118,7 @@ public class VerzekeringsmaatschappijManagerImpl implements Verzekeringsmaatscha
 		}
 		return false;
 	}
-	
+
 	@Override
 	public boolean addBehandelcode(Verzekeringsmaatschappij maatschappij, Verzekeringstype type, String behandelcode){
 		if(getVerzekeringstype(maatschappij, type.getNr()) != null){			
@@ -127,7 +129,7 @@ public class VerzekeringsmaatschappijManagerImpl implements Verzekeringsmaatscha
 		}
 		return false;
 	}
-	
+
 	@Override
 	public boolean deleteBehandelcode(Verzekeringsmaatschappij maatschappij, Verzekeringstype type, String behandelcode){
 		if(getVerzekeringstype(maatschappij, type.getNr()) != null){
@@ -137,7 +139,7 @@ public class VerzekeringsmaatschappijManagerImpl implements Verzekeringsmaatscha
 		}
 		return false;
 	}
-	
+
 	public void importData(ArrayList<Verzekeringsmaatschappij> lijst){
 		for(Verzekeringsmaatschappij maatschappij : lijst){
 			verzekeringsMaatschappijen.add(maatschappij);
@@ -152,37 +154,37 @@ public class VerzekeringsmaatschappijManagerImpl implements Verzekeringsmaatscha
 			String Postcode, String Plaats, String KVK, String RekeningNr) {
 		errorMessage = "";
 
-		
+
 		// Maatschappij nummer
 		if (!maatschappijnr.matches("([0-9]{3})")) {
 			if (maatschappijnr.length() < 1) {
 				errorMessage = errorMessage + "\nMaatschappij nummer niet ingevuld";
 			}
 			else{
-					errorMessage = errorMessage + "\nMaatschappij nummer niet correct";
+				errorMessage = errorMessage + "\nMaatschappij nummer niet correct";
 			}
 		}
-		
+
 		// Naam
-	if(!Naam.matches("([A-Z, a-z]{1,50})")){
-		if (Naam.length() < 1) {
-			errorMessage = errorMessage + "\nNaam niet ingevuld";
-		}else{
-			errorMessage = errorMessage + "\nEen naam kan alleen uit letters bestaan!";
+		if(!Naam.matches("([A-Z, a-z]{1,50})")){
+			if (Naam.length() < 1) {
+				errorMessage = errorMessage + "\nNaam niet ingevuld";
+			}else{
+				errorMessage = errorMessage + "\nEen naam kan alleen uit letters bestaan!";
+			}
 		}
-	}
-		
-		
+
+
 		// Adres	
-	if(!Adres.matches("([A-Z, a-z]{1,50})([ ]{1})([0-9]{1,9})")){
-		if (Adres.length() < 1) {
-			errorMessage = errorMessage + "\nAdres niet ingevuld";
-		}else{
-			errorMessage = errorMessage + "\nHet adres moet bestaan uit een straat gevolgd door een spatie en een nummer!";
+		if(!Adres.matches("([A-Z, a-z]{1,50})([ ]{1})([0-9]{1,9})")){
+			if (Adres.length() < 1) {
+				errorMessage = errorMessage + "\nAdres niet ingevuld";
+			}else{
+				errorMessage = errorMessage + "\nHet adres moet bestaan uit een straat gevolgd door een spatie en een nummer!";
+			}
 		}
-	}
-		
-		
+
+
 		// Postcode
 		if (!Postcode.matches("([0-9]{4})([A-Z]{2})")) {
 			if (Postcode.length() < 1) {
@@ -192,51 +194,51 @@ public class VerzekeringsmaatschappijManagerImpl implements Verzekeringsmaatscha
 				errorMessage = errorMessage + "\nPostcode niet correct \nHoofdletters gebruiken svp!";
 			}
 		}
-		
+
 		//Plaats
-	if(!Plaats.matches("([A-Z, a-z]{1,99})")){
-		if (Plaats.length() < 1) {
-			errorMessage = errorMessage + "\nWoonplaats niet ingevuld";
-		}else{
-			errorMessage = errorMessage + "\nEen plaatsnaam kan alleen uit letters bestaan!";
+		if(!Plaats.matches("([A-Z, a-z]{1,99})")){
+			if (Plaats.length() < 1) {
+				errorMessage = errorMessage + "\nWoonplaats niet ingevuld";
+			}else{
+				errorMessage = errorMessage + "\nEen plaatsnaam kan alleen uit letters bestaan!";
+			}
 		}
-	}
-		
-		
+
+
 		// KVK nummer
-	if(!KVK.matches("([0-9]{8})")){	
-		if (KVK.length() < 1) {
+		if(!KVK.matches("([0-9]{8})")){	
+			if (KVK.length() < 1) {
 				errorMessage = errorMessage + "\nKVK nummer niet ingevuld";
 			}
 			else{
 				errorMessage = errorMessage + "\nKVK nummer niet correct";
 			}
 		}
-		
+
 		// Rekening Nummer
-	if(!RekeningNr.matches("([0-9]{9})")){	
-		if (RekeningNr.length() < 1) {
+		if(!RekeningNr.matches("([0-9]{9})")){	
+			if (RekeningNr.length() < 1) {
 				errorMessage = errorMessage + "\nRekeningnummer niet ingevuld";
 			}
 			else{
 				errorMessage = errorMessage + "\nRekeningnummer niet correct";
 			}
 		}
-			return errorMessage;
+		return errorMessage;
 	}
 	@Override
 	public String checkType(String type, String naam, int eigenRisico) {
 		errorMessage = "";
-			
+
 		if (!type.matches("([0-9]{3})")) {
 			if (type.length() < 1) {
 				errorMessage = errorMessage + "\nType nummer niet ingevuld";
 			}
 			else{
-					errorMessage = errorMessage + "\nType nummer niet correct";
+				errorMessage = errorMessage + "\nType nummer niet correct";
 			}
 		}
-		
+
 		if(!naam.matches("[A-Z, a-z]{1,50}")){
 			if(naam.length() < 1){
 				errorMessage = errorMessage + "\nNaam niet ingevuld";				
@@ -244,8 +246,20 @@ public class VerzekeringsmaatschappijManagerImpl implements Verzekeringsmaatscha
 				errorMessage = errorMessage + "\nNaam niet correct";
 			}
 		}	
-				
-		
+
+
 		return errorMessage;
+	}
+
+	public boolean checkKvk(Verzekeringsmaatschappij maatschappij){
+		boolean state = false;
+		for (Verzekeringsmaatschappij mts: verzekeringsMaatschappijen){
+			if(mts.getKVK() == (maatschappij.getKVK())){
+				state = false;
+			}else {
+				state = true;
+			}
+		}
+		return state;
 	}
 }
